49aa709f33abd8b3c187a5b8d0c670dff5367a69
Ng <benjaminva@gmail.com>
2020-09-29 18:08:53 -0500

    agrega casos de prueba SpalyTree

--- Act 3.3 - Árbol Desplegado: Implementando un Splay Tree/main.cpp
+++ Act 3.3 - Árbol Desplegado: Implementando un Splay Tree/main.cpp
@@ -1,97 +1,101 @@
 /*
  * main.cpp
  *
- *  Created on: 10/08/2020
- *      Author: bvaldesa
+ *  Created on: 30/10/2020
+ *      Author: clase
  */
+
 #include <iostream>
-#include <sstream>
 #include <cstring>
-#include <string>
-#include "heap.h"
+#include "unit.h"
+#include "splay.h"
+
+/*
+template <class T>
+void Node<T>::print_tree(std::stringstream &aux) const {
+	if (parent != 0){
+		aux << "\n node " << value;
+		aux << " parent " << parent->value;
+	}else
+		aux << "\n root " << value;
+	if (left != 0)
+		aux << " left " << left->value;
+	if (right != 0)
+		aux << " right " << right->value;
+	aux << "\n";
+
+	if (left != 0) {
+		left->print_tree(aux);
+	}
+	if (right != 0) {
+		right->print_tree(aux);
+	}
+}
+
+template <class T>
+void Node<T>::preorder(std::stringstream &aux) const {
+	aux << value;
+	if (left != 0) {
+		aux << " ";
+		left->preorder(aux);
+	}
+	if (right != 0) {
+		aux << " ";
+		right->preorder(aux);
+	}
+}
+
+*/
+
+
 
 using namespace std;
 
 int main(int argc, char* argv[]) {
-	Heap<int> heap(20);
-	string ans;
-	heap.push(8);
+  string in_ans = "", pre_ans = "";
+  SplayTree<int> my_splay;
+  bool found = 0;
 
-  ans =	"[8]";
-	cout << " 1 " <<	(!ans.compare(heap.toString()) ? "success\n" : "fail\n");
+  my_splay.add(15);
+  in_ans =	"[15]";
+  cout << " 1 " <<	(!in_ans.compare(my_splay.inorder()) ? "success\n" : "fail\n");
 
-	heap.push(20);
-	heap.push(31);
-	heap.push(5);
-	heap.push(87);
-	ans ="[5 8 31 20 87]";
-	cout << " 2 " <<	(!ans.compare(heap.toString()) ? "success\n" : "fail\n");
+  my_splay.add(10);
+  my_splay.add(17);
+  my_splay.add(7);
+  my_splay.add(13);
+  my_splay.add(16);
+  in_ans =	"[7 10 13 15 16 17]";
+  pre_ans =	"[16 13 7 10 15 17]";
+  cout << " 2 " <<	(!in_ans.compare(my_splay.inorder()) ? "success\n" : "fail\n");
+  cout << " 2 " <<	(!pre_ans.compare(my_splay.preorder()) ? "success\n" : "fail\n");
 
-	heap.pop();
-	ans ="[8 20 31 87]";
-	cout << " 3 " <<	(!ans.compare(heap.toString()) ? "success\n" : "fail\n");
+  found = my_splay.find(15);
+  in_ans =	"[7 10 13 15 16 17]";
+  pre_ans =	"[15 13 7 10 16 17]";
+  cout << " 3 " <<	(!in_ans.compare(my_splay.inorder()) ? "success\n" : "fail\n");
+  cout << " 3 " <<	(!pre_ans.compare(my_splay.preorder()) ? "success\n" : "fail\n");
+  cout << " 3 " <<	(found == 1 ? "success\n" : "fail\n");
 
-	cout << " 4 " <<	((4 == heap.size()) ? "success\n" : "fail\n");
+  found = my_splay.find(17);
+  in_ans =	"[7 10 13 15 16 17]";
+  pre_ans =	"[17 16 15 13 7 10]";
+  cout << " 4 " <<	(!in_ans.compare(my_splay.inorder()) ? "success\n" : "fail\n");
+  cout << " 4 " <<	(!pre_ans.compare(my_splay.preorder()) ? "success\n" : "fail\n");
+  cout << " 4 " <<	(found == 1 ? "success\n" : "fail\n");
 
-	heap.pop();
-	ans ="[20 87 31]";
-	cout << " 5 " <<	(!ans.compare(heap.toString()) ? "success\n" : "fail\n");
+  my_splay.remove(13);  //bottum up
+  in_ans =	"[7 15 16 17]";
+  pre_ans =	"[15 7 16 17]";
+  cout << " 5 " <<	(!in_ans.compare(my_splay.inorder()) ? "success\n" : "fail\n");
+  cout << " 5 " <<	(!pre_ans.compare(my_splay.preorder()) ? "success\n" : "fail\n");
+  cout << " 5 " <<	(found == 1 ? "success\n" : "fail\n");
 
-	cout << " 6 " <<	((3 == heap.size()) ? "success\n" : "fail\n");
+  my_splay.remove(16);  //bottum up
+  in_ans =	"[7 15 17]";
+  pre_ans =	"[15 7 17]";
+  cout << " 6 " <<	(!in_ans.compare(my_splay.inorder()) ? "success\n" : "fail\n");
+  cout << " 6 " <<	(!pre_ans.compare(my_splay.preorder()) ? "success\n" : "fail\n");
+  cout << " 6 " <<	(found == 1 ? "success\n" : "fail\n");
 
-	cout << " 7 " <<	((20 == heap.top()) ? "success\n" : "fail\n");
-
-	cout << " 8 " <<	((0 == heap.empty()) ? "success\n" : "fail\n");
-
-	heap.pop();
-
-	cout << " 9 " <<	((31 == heap.top()) ? "success\n" : "fail\n");
-
-	heap.pop();
-
-	cout << " 10 " <<	((0 == heap.empty()) ? "success\n" : "fail\n");
-
-/*
-push
-Un entero, el cual se desea agregar a la fila priorizada
-
-pop
-Saca de la fila priorizada el dato que tiene mayor prioridad
-
-top
-Regresa el valor del dato que esta con mayor prioridad en la fila priorizada
-
-empty
-Regresa un valor boleando diciendo si la fila priorizada esta vacía o tiene datos.
-
-size
-Un valor entero que represena la cantidad de datos de la fila priorizada
-
-	BST<int> bst;
-	string  ans;
-
-	bst.add(10);
-
-	cout << " 2 " <<	(1 == bst.height() ? "success\n" : "fail\n");
-
-	bst.add(12);
-	bst.add(8);
-	bst.add(110);
-	bst.add(112);
-	bst.add(18);
-	ans = "[10 8 12 110 18 112]\n[8 10 12 18 110 112]\n[8 18 112 110 12 10]\n[10 8 12 110 18 112]";
-	cout << " 3 " <<	(!ans.compare(bst.visit()) ? "success\n" : "fail\n");
-
-	cout << " 4 " <<	(4 == bst.height() ? "success\n" : "fail\n");
-
-	ans = "[10 12 110]";
-	cout << " 5 " <<	(!ans.compare(bst.ancestors(112)) ? "success\n" : "fail\n");
-
-	ans = "[]";
-	cout << " 6 " <<	(!ans.compare(bst.ancestors(1000)) ? "success\n" : "fail\n");
-
-	cout << " 7 " <<	(4 == bst.whatlevelamI(18) ? "success\n" : "fail\n");
-
-	cout << " 8 " <<	(2 == bst.whatlevelamI(8) ? "success\n" : "fail\n");
-*/
 }
