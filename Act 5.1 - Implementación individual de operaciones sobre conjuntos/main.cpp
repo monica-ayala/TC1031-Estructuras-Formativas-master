54e1077b36890b562cca0d410eb3de7ca80d03b1
Ng <benjaminva@gmail.com>
2021-07-27 20:59:10 -0500

    operaciones sobre conjuntos

--- Act 5.1 - Implementación individual de operaciones sobre conjuntos/main.cpp
+++ Act 5.1 - Implementación individual de operaciones sobre conjuntos/main.cpp
@@ -1,9 +1,10 @@
 /*
- * main.cpp
- *
- *  Created on: 29/10/2020
- *      Author: clase
- */
+*  autor: Benjamin Valdes Aguirre
+*  fecha: Agosto 2021
+*  programa: act 5.1 Implementacion Heap
+*  desc: estos programas son solciones parciales a algunos de
+*  las actividades planteadas en el curso TC1031
+*/
 
 #include <iostream>
 #include <cstring>
@@ -28,23 +29,6 @@ std::string Quadratic<Key, Value>::toString() const {
 }
 */
 
-/* // impresion en la clase chain
-template <class Key, class Value>
-std::string Chain<Key, Value>::toString() const {
-	std::stringstream aux;
-	for (int i = 0; i < size; i++){
-			if (!keys[i].empty()){
-				aux <<"(" << i ;
-				for (int j = 0; j < keys[i].size(); j ++){
-						 aux << " [" << keys[i][j] << " : " << values[i][j] << "]";
-				}
-				aux <<") ";
-			}
-	}
-	return aux.str();
-}
-*/
-
 unsigned int myHash(const string s) {
 	unsigned int acum = 0;
 	for (unsigned int i = 0; i < s.size(); i++) {
@@ -55,10 +39,10 @@ unsigned int myHash(const string s) {
 
 int main(int argc, char* argv[]) {
 
-	    Chain <string, int> chain_hash(10, myHash);
 	    Quadratic <string, int> quad_hash(10, string("empty"), myHash);
 
-	    string c_ans, q_ans;
+			int ans;
+	    string q_ans;
 
 			quad_hash.put(string("tecnica"), 11);
 			quad_hash.put(string("casa"), 12);
@@ -68,33 +52,26 @@ int main(int argc, char* argv[]) {
 			quad_hash.put(string("operador"), 18);
 			quad_hash.put(string("taza"), 18);
 
-			chain_hash.put(string("tecnica"), 11);
-			chain_hash.put(string("casa"), 12);
-			chain_hash.put(string("caza"), 13);
-			chain_hash.put(string("amos"), 15);
-			chain_hash.put(string("magneto"), 17);
-			chain_hash.put(string("operador"), 18);
-			chain_hash.put(string("taza"), 18);
-
-			c_ans = "(0 [operador : 18]) (2 [amos : 15] [taza : 18]) (5 [caza : 13]) (7 [tecnica : 11] [magneto : 17]) (8 [casa : 12]) ";
 			q_ans = "(0 operador : 18) (1 magneto : 17) (2 amos : 15) (3 taza : 18) (5 caza : 13) (7 tecnica : 11) (8 casa : 12) ";
 
-	    cout << " 1 " <<	( (!c_ans.compare(chain_hash.toString().c_str())) ? "success\n" : "fail\n");
-
-			cout << " 2 " <<	( (!q_ans.compare(quad_hash.toString().c_str())) ? "success\n" : "fail\n");
+			cout << " 1 " <<	( (!q_ans.compare(quad_hash.toString().c_str())) ? "success\n" : "fail\n");
 
 			quad_hash.put(string("max"), 99);
 			quad_hash.put(string("cocacola"), 188);
 
-			chain_hash.put(string("max"), 99);
-			chain_hash.put(string("cocacola"), 188);
-
-			c_ans = "(0 [operador : 18]) (1 [cocacola : 188]) (2 [amos : 15] [taza : 18]) (5 [caza : 13]) (6 [max : 99]) (7 [tecnica : 11] [magneto : 17]) (8 [casa : 12]) ";
 			q_ans = "(0 operador : 18) (1 magneto : 17) (2 amos : 15) (3 taza : 18) (5 caza : 13) (6 max : 99) (7 tecnica : 11) (8 casa : 12) ";
 
-			cout << " 3 " <<	( (!c_ans.compare(chain_hash.toString().c_str())) ? "success\n" : "fail\n");
+			cout << " 2 " <<	( (!q_ans.compare(quad_hash.toString().c_str())) ? "success\n" : "fail\n");
 
-			cout << " 4 " <<	( (!q_ans.compare(quad_hash.toString().c_str())) ? "success\n" : "fail\n");
+			ans = 12;
+
+			cout << " 3 " <<	( ans == quad_hash.get("casa") ? "success\n" : "fail\n");
+
+			quad_hash.put(string("concha"), 16);
+
+			ans = 17;
+
+			cout << " 4 " <<	( ans == quad_hash.get("magneto") ? "success\n" : "fail\n");
 
 			return 0;
 }
