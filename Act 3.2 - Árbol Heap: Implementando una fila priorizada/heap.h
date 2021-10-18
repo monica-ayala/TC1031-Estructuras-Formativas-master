da29b3d6398b34f6455691e382f83e1b52f44244
Monica Ayala Marrero <a01707439@itesm.mx>
2021-10-18 15:47:36 +0000

    heap complete

--- Act 3.2 - Árbol Heap: Implementando una fila priorizada/heap.h
+++ Act 3.2 - Árbol Heap: Implementando una fila priorizada/heap.h
@@ -1,79 +1,61 @@
 #ifndef HEAP_H_
 #define HEAP_H_
 
-#include <string>
-#include <sstream>
 #include <iostream>
+
+using namespace std;
 
 template <class T>
 class Heap {
 private:
 	T *data;
-	unsigned int sizeList;
-	unsigned int count;
-	unsigned int parent(unsigned int) const;
-	unsigned int left(unsigned int) const;
-	unsigned int right(unsigned int) const;
-	void heapify(unsigned int);
-	void swap(unsigned int, unsigned int);
+	int sizeList;
+	int count;
+	int parent(int pos);
+	int left(int pos);
+	int right(int pos);
+	void swap(int i, int j);
+  void heapify(int pos);
+
 public:
-	Heap(unsigned int);
-	~Heap();
-	bool empty() const;
-	bool full() const;
-	void push(T);
-    int size() const {return count;}
+	Heap(int sze);
+	string toString();
+
+//funciones de tarea
+  void push(T);
 	T pop();
-    T top();
-	void clear();
-	std::string toString() const;
+  T top();
+  bool empty();
+  int size();
+	
 };
 
 template <class T>
-Heap<T>::Heap(unsigned int sze)  {
-	sizeList = sze;
-	data = new T[sizeList];
-	count = 0;
-}
-template <class T>
-Heap<T>::~Heap() {
-	delete [] data; data = 0;
-	sizeList = 0; count = 0;
-}
-template <class T>
-bool Heap<T>::empty() const {
-	return (count == 0);
-}
-template <class T>
-bool Heap<T>::full() const {
-	return (count == sizeList);
-}
-template <class T>
-unsigned int Heap<T>::parent(unsigned int pos) const {
+int Heap<T>::parent(int pos){
 	return (pos - 1) / 2;
 }
 
 template <class T>
-unsigned int Heap<T>::left(unsigned int pos) const {
+int Heap<T>::left(int pos){
 	return ((2 * pos) + 1);
 }
 
 template <class T>
-unsigned int Heap<T>::right(unsigned int pos) const {
+int Heap<T>::right(int pos){
 	return ((2 * pos) + 2);
 }
 
 template <class T>
-void Heap<T>::swap(unsigned int i, unsigned int j) {
+void Heap<T>::swap(int i, int j){
 	T aux = data[i];
 	data[i] = data[j];
 	data[j] = aux;
 }
 template <class T>
-void Heap<T>::heapify(unsigned int pos) {
-	unsigned int le = left(pos);
-	unsigned int ri = right(pos);
-	unsigned int min = pos;
+void Heap<T>::heapify(int pos){
+	int le = left(pos);
+	int ri = right(pos);
+	int min = pos;
 	if (le <= count && data[le] < data[min]) {
 		min = le;
 	}
@@ -87,9 +69,32 @@ void Heap<T>::heapify(unsigned int pos) {
 }
 
 template <class T>
+Heap<T>::Heap(int sze){
+	sizeList = sze;
+	data = new T[sizeList];
+	count = 0;
+}
+
+template <class T>
+string Heap<T>::toString(){
+	stringstream aux;
+	aux << "[";	for (int i = 0; i < count; i++) {
+		if (i != 0) {
+			aux << " ";
+		} aux << data[i];
+	} aux << "]";
+	return aux.str();
+}
+
+template <class T>
+bool Heap<T>::empty(){
+	return (count == 0);
+}
+
+template <class T>
 void Heap<T>::push(T val){
-	if (!full()) {
-		unsigned int pos;
+	if (!(count == sizeList)) {
+		int pos;
 		pos = count;
 		count++;
 		while (pos > 0 && val < data[parent(pos)]) {
@@ -119,19 +124,8 @@ T Heap<T>::top(){
 }
 
 template <class T>
-void Heap<T>::clear() {
-	count = 0;
-}
-
-template <class T>
-std::string Heap<T>::toString() const {
-	std::stringstream aux;
-	aux << "[";	for (unsigned int i = 0; i < count; i++) {
-		if (i != 0) {
-			aux << " ";
-		} aux << data[i];
-	} aux << "]";
-	return aux.str();
+int Heap<T>::size(){
+	return count;
 }
 
 #endif /* HASH_H_ */
